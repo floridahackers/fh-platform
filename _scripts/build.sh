19fdee8d37bf54fc50d7a46da52140f96b73b7ea
Juan Alvarado <jalvarado91@gmail.com>
2017-07-14 19:42:18 -0400

    Change deploy target repo

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,7 +2,7 @@
 
 echo "Initializing git"
 git init
-git remote add deploy "deploy@floridahackers.com:$fh_site_path"
+git remote add deploy "deploy@floridahackers.com:/var/www/floridahackers.com/html/deploy.git"
 git config user.name "Travis CI"
 git config user.email "fh+travisCI@gmail.com"
 

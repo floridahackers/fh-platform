69af1af97a3b32fccf32b44fae2666d229d6a7a7
Juan Alvarado <jalvarado91@gmail.com>
2017-07-14 19:15:17 -0400

    Force on deploy branch
    Force push on deploy branch. Since it's basically a throw away branch.

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,7 +3,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
 	echo "Deploying to remote"
 	git add .
 	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
-	git push deploy master:deploy
+	git push --force deploy master:deploy
 else
 	echo "Not deploying, since this branch isn't master."
 fi

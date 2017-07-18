4979d27ccc397d6cc2a6599b0f84d00043274c0e
Juan Alvarado <jalvarado91@gmail.com>
2017-07-18 19:43:20 -0400

    Correct path

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,7 +2,7 @@
 
 echo "Initializing git"
 git init
-git remote add deploy "deploy@floridahackers.com:/var/www/floridahackers.com/.git"
+git remote add deploy "deploy@floridahackers.com:/var/www/floridahackers.com/deploy/.git"
 git config user.name "Travis CI"
 git config user.email "fh+travisCI@gmail.com"
 

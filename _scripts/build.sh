b63c26d135895c2eab68c72c144b540c071c958f
Juan Alvarado <jalvarado91@gmail.com>
2017-07-18 19:34:36 -0400

    Modified travis scripts

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -2,12 +2,20 @@
 
 echo "Initializing git"
 git init
-git remote add deploy "deploy@floridahackers.com:/var/www/floridahackers.com/html/deploy.git"
+git remote add deploy "deploy@floridahackers.com:/var/www/floridahackers.com/.git"
 git config user.name "Travis CI"
 git config user.email "fh+travisCI@gmail.com"
 
 echo "Fetching from remote"
 git fetch deploy
+git checkout -b build
 
 echo "Building..."
 npm run build
+
+echo "Commiting build"
+git add .
+git commit -q -m "Build #$TRAVIS_BUILD_NUMBER"
+
+git checkout master
+git merge -X theirs --commit -m "Merge build #$TRAVIS_BUILD_NUMBER" build
--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,7 +3,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
 	echo "Deploying to remote"
 	git add .
 	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
-	git push --force deploy master:deploy
+	git push deploy master
 else
 	echo "Not deploying, since this branch isn't master."
 fi

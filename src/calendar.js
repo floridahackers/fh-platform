4163963f4afd35844283e48360537194cb40537d
Juan Alvarado <jalvarado91@gmail.com>
2017-07-18 21:01:57 -0400

    Remove unused styles, minor copy update

--- src/calendar.js
+++ src/calendar.js
@@ -1,37 +1,47 @@
-require('./components/shared');
-import React from 'react';
-import { render } from 'react-dom';
-import moment from 'moment';
-import UpcomingEvents from './components/UpcomingEvents';
-import PastEvents from './components/PastEvents';
+require("./components/shared");
+import React from "react";
+import { render } from "react-dom";
+import moment from "moment";
+import UpcomingEvents from "./components/UpcomingEvents";
+import PastEvents from "./components/PastEvents";
 
-import all_events from './all_events';
+import all_events from "./all_events";
 
 let event_sort_fn = (a, b) => {
-	return b.date.from - a.date.from
-}
+  return b.date.from - a.date.from;
+};
 let event_sort_fn_new = (a, b) => {
-	return a.date.from - b.date.from
-}
+  return a.date.from - b.date.from;
+};
 
-let old_events = all_events.filter(event => event.date.to - Date.now() < 0 && event.approved);
-let new_events = all_events.filter(event => event.date.to - Date.now() > 0 && event.approved);
+let old_events = all_events.filter(
+  event => event.date.to - Date.now() < 0 && event.approved
+);
+let new_events = all_events.filter(
+  event => event.date.to - Date.now() > 0 && event.approved
+);
 
 new_events.sort(event_sort_fn_new);
 old_events.sort(event_sort_fn);
 
-
 class CalendarApp extends React.Component {
   render() {
     return (
-    	<div>
-      		<UpcomingEvents events={new_events} />
-      		<h2 style={{marginTop: "36px"}}>Past Events</h2>
-      		<PastEvents events={old_events} />
-		</div>
+      <div>
+				<h1>Upcoming Events</h1>
+        {new_events.length > 0
+          ? <div>
+              <p>Here are some events you can attend this year!</p>
+              <UpcomingEvents events={new_events} />
+            </div>
+          : <div>
+              <h3>No upcoming events at the moment.</h3>
+            </div>}
+        <h2 style={{ marginTop: "36px" }}>Past Events</h2>
+        <PastEvents events={old_events} />
+      </div>
     );
   }
 }
 
-render(<CalendarApp />, 
-  document.querySelector('.app-container'));
+render(<CalendarApp />, document.querySelector(".app-container"));
--- src/index.js
+++ src/index.js
@@ -1,28 +1,39 @@
-require('./components/shared');
-import React from 'react';
-import { render } from 'react-dom';
-import EventCard from './components/EventCard';
-import all_events from './all_events';
+require("./components/shared");
+import React from "react";
+import { render } from "react-dom";
+import EventCard from "./components/EventCard";
+import all_events from "./all_events";
 
 let event_sort_fn_new = (a, b) => {
-  return a.date.from - b.date.from
-}
-let new_events = all_events.filter(event => event.date.to - Date.now() > 0 && event.approved);
+  return a.date.from - b.date.from;
+};
+let new_events = all_events.filter(
+  event => event.date.to - Date.now() > 0 && event.approved
+);
 new_events.sort(event_sort_fn_new);
 
 class HomeApp extends React.Component {
   render() {
     return (
       <div>
-        { new_events.length > 0 ?
-            new_events.slice(0,2).map((event, key) => <EventCard key={key} info={event}/>)
-            : <h3>No upcoming events right now.</h3>
-        }
+        {new_events.length > 0
+          ? <div>
+              {""}{new_events
+                .slice(0, 2)
+                .map((event, key) => <EventCard key={key} info={event} />)}
+              <a className="card-shadow all-action" href="calendar.html">
+                See all events <span>&rarr;</span>
+              </a>
+            </div>
+          : <div>
+              <h4>No upcoming events right now.</h4>
+              <a className="card-shadow all-action" href="calendar.html">
+                See past events. <span>&rarr;</span>
+              </a>
+            </div>}
       </div>
     );
   }
 }
 
-
-render(<HomeApp />, 
-  document.querySelector('.events-app'));
+render(<HomeApp />, document.querySelector(".events-app"));
